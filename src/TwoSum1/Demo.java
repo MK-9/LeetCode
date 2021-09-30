53a54baeeda68239158e0f327696fc10bbfab19b
DESKTOP-AMPP2BT\Mking <msit26055@gmail.com>
2021-10-01 00:35:05 +0330

    add Optimum solution for 1. Two Sum

--- src/TwoSum1/Demo.java
+++ src/TwoSum1/Demo.java
@@ -1,15 +1,18 @@
 package TwoSum1;
 
+import java.util.Arrays;
+import java.util.HashMap;
+
 public class Demo {
 
     public static void main(String[] arg) {
-
+        System.out.println(Arrays.toString(twoSumOptimumSolution(new int[]{1, 3, 4, 2}, 6)));
     }
 
     /**
      * Brute Force Solution
      */
-    public int[] twoSum(int[] nums, int target) {
+    public static int[] twoSum(int[] nums, int target) {
 
         int[] output = new int[2];
 
@@ -24,5 +27,25 @@ public class Demo {
         }
 
         return output;
+    }
+
+    public static int[] twoSumOptimumSolution(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            int rem = target - nums[i];
+            if (map.containsKey(rem)) {
+                int index = map.get(rem);
+                if (index == i)
+                    continue;
+                return new int[]{i, index};
+            }
+        }
+
+        return new int[]{};
     }
 }
