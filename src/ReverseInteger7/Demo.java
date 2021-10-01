a8342e55046ca45b39de1c1d81bb20b49012c617
DESKTOP-AMPP2BT\Mking <msit26055@gmail.com>
2021-10-01 23:54:08 +0330

    init solution

---
+++ src/ReverseInteger7/Demo.java
@@ -0,0 +1,14 @@
+package ReverseInteger7;
+
+import java.util.Arrays;
+
+public class Demo {
+
+    public static void main(String[] arg) {
+//        System.out.println(Arrays.toString(twoSumOptimumSolution(new int[]{1, 3, 4, 2}, 6)));
+    }
+
+    public int reverse(int x) {
+
+    }
+}
