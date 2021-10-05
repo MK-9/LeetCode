abe318adff22aa0e7cc5135ce2892b3d383b9ca1
DESKTOP-AMPP2BT\Mking <msit26055@gmail.com>
2021-10-05 23:32:09 +0330

    initial palindrome solution

---
+++ src/Palindrome/Solution.java
@@ -0,0 +1,18 @@
+package Palindrome;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome(12355321));
+    }
+
+
+    public static boolean isPalindrome(int x) {
+
+
+        
+
+
+        return false;
+    }
+}
