70198fcd90d9038922bc005535c2ce375c082700
DESKTOP-AMPP2BT\Mking <msit26055@gmail.com>
2021-10-06 00:04:23 +0330

    finish bruteForce Solution for Palindrome

--- src/Palindrome/Solution.java
+++ src/Palindrome/Solution.java
@@ -3,16 +3,34 @@ package Palindrome;
 public class Solution {
 
     public static void main(String[] args) {
-        System.out.println(isPalindrome(12355321));
+        System.out.println(isPalindromeBruteForce(1255251));
     }
 
+    public static boolean isPalindromeBruteForce(int x) {
+        if (x < 0)
+            return false;
+        return reverseInteger(x) == x;
+    }
 
-    public static boolean isPalindrome(int x) {
+    public static int reverseInteger(int x) {
+        boolean neg = x < 0;
 
+        if (x == -x) {
+            return 0;
+        }
 
-        
+        long rem = 0;
+        long output = 0;
+        do {
+            output *= 10L;
+            rem = x % 10;
+            output += rem;
+            x = x / 10;
+        } while (x != 0);
 
+        if (output > Integer.MAX_VALUE)
+            return 0;
 
-        return false;
+        return neg ? (int) -output : (int) output;
     }
 }
