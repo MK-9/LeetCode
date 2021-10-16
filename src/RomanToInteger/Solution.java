6630a9640eb28ea9811c5dada3952e0dcd6200e6
DESKTOP-AMPP2BT\Mking <msit26055@gmail.com>
2021-10-16 18:19:13 +0330

    add solution for roman to integer problem

--- src/RomanToInteger/Solution.java
+++ src/RomanToInteger/Solution.java
@@ -1,15 +1,16 @@
 package RomanToInteger;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class Solution {
 
     public static void main(String[] args) {
-        System.out.println(romanToInt("III"));
-        System.out.println(romanToInt("IV"));
-        System.out.println(romanToInt("LVIII"));
-        System.out.println(romanToInt("IX"));
-        System.out.println(romanToInt("MCMXCIV"));
+        System.out.println(romanToInts("III"));
+        System.out.println(romanToInts("IV"));
+        System.out.println(romanToInts("LVIII"));
+        System.out.println(romanToInts("IX"));
+        System.out.println(romanToInts("MCMXCIV"));
     }
 
     public static int romanToInt(String s) {
@@ -26,26 +27,52 @@ public class Solution {
         long output = 0;
         long oldDigit = 0;
         long newDigit;
-        long temp = 0;
+        long temp;
+
         for (int i = 0; i < s.length(); i++) {
             char character = s.charAt(i);
 
             if (i == 0) {
-                oldDigit = romans.get(character);
-                output = oldDigit;
+                newDigit = romans.get(character);
+                oldDigit = newDigit;
+                if (s.length() == 1) output = newDigit;
                 continue;
             }
 
             newDigit = romans.get(character);
 
-            if (oldDigit < newDigit) {
-                temp =  newDigit - oldDigit;
+            if (oldDigit >= newDigit) {
+                output += oldDigit;
+                oldDigit = newDigit;
+            } else if (oldDigit != 0) {
+                temp = newDigit - oldDigit;
+                output += temp;
+                oldDigit = 0;
             } else {
-                output = output + newDigit + oldDigit;
+                oldDigit = newDigit;
             }
-
         }
 
         return (int) output;
+    }
+
+    public static int romanToInts(String s) {
+        Map<Character, Integer> m = new HashMap<>();
+        m.put('I', 1);
+        m.put('V', 5);
+        m.put('X', 10);
+        m.put('L', 50);
+        m.put('C', 100);
+        m.put('D', 500);
+        m.put('M', 1000);
+        int res = m.get(s.charAt(s.length()-1));
+        for(int i =s.length()-2; i>=0; i--)
+        {
+            if(m.get(s.charAt(i))<m.get(s.charAt(i+1)))
+                res -= m.get(s.charAt(i));
+            else
+                res += m.get(s.charAt(i));
+        }
+        return res;
     }
 }
