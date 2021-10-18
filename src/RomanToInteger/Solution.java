c80d63a62d991eb35f74df92a6564e46bbbfbb89
DESKTOP-AMPP2BT\Mking <msit26055@gmail.com>
2021-10-18 23:56:03 +0330

    remove extra solution

--- src/RomanToInteger/Solution.java
+++ src/RomanToInteger/Solution.java
@@ -6,57 +6,14 @@ import java.util.Map;
 public class Solution {
 
     public static void main(String[] args) {
-        System.out.println(romanToInts("III"));
-        System.out.println(romanToInts("IV"));
-        System.out.println(romanToInts("LVIII"));
-        System.out.println(romanToInts("IX"));
-        System.out.println(romanToInts("MCMXCIV"));
+        System.out.println(romanToInt("III"));
+        System.out.println(romanToInt("IV"));
+        System.out.println(romanToInt("LVIII"));
+        System.out.println(romanToInt("IX"));
+        System.out.println(romanToInt("MCMXCIV"));
     }
 
     public static int romanToInt(String s) {
-        //create romans number
-        HashMap<Character, Integer> romans = new HashMap<>();
-        romans.put('I', 1);
-        romans.put('V', 5);
-        romans.put('X', 10);
-        romans.put('L', 50);
-        romans.put('C', 100);
-        romans.put('D', 500);
-        romans.put('M', 1000);
-
-        long output = 0;
-        long oldDigit = 0;
-        long newDigit;
-        long temp;
-
-        for (int i = 0; i < s.length(); i++) {
-            char character = s.charAt(i);
-
-            if (i == 0) {
-                newDigit = romans.get(character);
-                oldDigit = newDigit;
-                if (s.length() == 1) output = newDigit;
-                continue;
-            }
-
-            newDigit = romans.get(character);
-
-            if (oldDigit >= newDigit) {
-                output += oldDigit;
-                oldDigit = newDigit;
-            } else if (oldDigit != 0) {
-                temp = newDigit - oldDigit;
-                output += temp;
-                oldDigit = 0;
-            } else {
-                oldDigit = newDigit;
-            }
-        }
-
-        return (int) output;
-    }
-
-    public static int romanToInts(String s) {
         Map<Character, Integer> m = new HashMap<>();
         m.put('I', 1);
         m.put('V', 5);
