4a3fab5057159bfe80c5e7301e738ce52427e5d5
DESKTOP-AMPP2BT\Mking <msit26055@gmail.com>
2021-10-20 13:38:56 +0330

    initial longest common prefix solution

---
+++ src/LongestCommmonsPrefix/Solution.java
@@ -0,0 +1,15 @@
+package LongestCommmonsPrefix;
+
+public class Solution {
+
+    public static void main(String[] arg){
+
+    }
+
+    public static String longestCommonPrefix(String[] strs) {
+        String output = "";
+
+        return output;
+    }
+
+}
--- src/RomanToInteger/Solution.java
+++ src/RomanToInteger/Solution.java
@@ -22,10 +22,9 @@ public class Solution {
         m.put('C', 100);
         m.put('D', 500);
         m.put('M', 1000);
-        int res = m.get(s.charAt(s.length()-1));
-        for(int i =s.length()-2; i>=0; i--)
-        {
-            if(m.get(s.charAt(i))<m.get(s.charAt(i+1)))
+        int res = m.get(s.charAt(s.length() - 1));
+        for (int i = s.length() - 2; i >= 0; i--) {
+            if (m.get(s.charAt(i)) < m.get(s.charAt(i + 1)))
                 res -= m.get(s.charAt(i));
             else
                 res += m.get(s.charAt(i));
