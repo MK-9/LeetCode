abe542903efa548a0a9ec588232b50242d6dd7a0
DESKTOP-AMPP2BT\Mking <msit26055@gmail.com>
2021-10-22 14:00:31 +0330

    add bruteForce solution

--- src/LongestCommmonsPrefix/Solution.java
+++ src/LongestCommmonsPrefix/Solution.java
@@ -2,14 +2,34 @@ package LongestCommmonsPrefix;
 
 public class Solution {
 
-    public static void main(String[] arg){
-
+    public static void main(String[] arg) {
+        String[] strs = new String[]{"flower", "flow", "flowa"};
+        System.out.println(longestCommonPrefixBruteForceSolution(strs));
     }
 
-    public static String longestCommonPrefix(String[] strs) {
-        String output = "";
+    public static String longestCommonPrefixBruteForceSolution(String[] strs) {
+        if (strs.length == 1)
+            return strs[0];
 
-        return output;
+        StringBuilder output = new StringBuilder();
+        String first = strs[0];
+
+        for (int k = 0; k < first.length(); k++) {
+            for (int i = 1; i < strs.length; i++) {
+                String word = strs[i];
+                if (k == word.length()) {
+                    return output.toString();
+                }
+
+                if (first.charAt(k) == word.charAt(k)) {
+                    if (i == strs.length - 1)
+                        output.append(word.charAt(k));
+                } else {
+                    return output.toString();
+                }
+            }
+        }
+        return output.toString();
     }
 
 }
